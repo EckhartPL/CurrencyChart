d1867c0d6b6eee410cb88cd7c5abc52da498190d
Mateusz <mateusz.masek@wp.pl>
2022-04-23 16:46:57 +0200

    Fixed viewed information about price change

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 App features:
 * Checking single currency weighted average price and getting information about if price rised or decreased,
-* Live chart refreshing every 5 seconds.
+* Live chart refreshing every 10 seconds.
--- src/component/CryptoPriceOfOnePair.tsx
+++ src/component/CryptoPriceOfOnePair.tsx
@@ -6,9 +6,19 @@ interface Props {
 }
 
 export const CryptoPriceOfOnePair = (props: Props) => (
-    <p style={{backgroundColor: Number(props.onePair.priceChangePercent) > 0 ? 'green' : Number(props.onePair.priceChangePercent) < 0 ? 'red' : 'lightblue', fontSize:'20px'}}>
+    <p style={{
+        backgroundColor: Number(props.onePair.priceChangePercent) > 0
+            ? 'green'
+            : Number(props.onePair.priceChangePercent) < 0
+                ? 'red'
+                : 'lightblue', fontSize: '20px'
+    }}>
         {props.onePair.symbol} current weighted value: {props.onePair.weightedAvgPrice}.
         <br />
-        Price <strong>{Number(props.onePair.priceChangePercent) > 0 ? 'raised' : 'dropped'}</strong> by {Number(props.onePair.priceChangePercent)}%
+        Price {Number(props.onePair.priceChangePercent) > 0
+            ? `raised by ${Number(props.onePair.priceChangePercent)}%`
+            : Number(props.onePair.priceChangePercent) < 0
+                ? `dropped by ${Number(props.onePair.priceChangePercent)}%`
+                : 'didn\'t change.'}
     </p>
 );
